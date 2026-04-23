52eb7eaacf3b8f2abbce625d38eb07b01f585169
atomrobic <77220996+atomrobic@users.noreply.github.com>
2026-04-23 15:14:21 +0530

    chore: setup project structure for backend and frontend - Add Python virtual environment (venv) inside toddy_shop_backend - Add requirements.txt with Django and DRF dependencies - Create toddy_shop_frontend folder for Next.js (upcoming) - Update .gitignore for Django, Node, venv, and env files

---
+++ toddy_shop_backend/toddy_shop_backend/asgi.py
@@ -0,0 +1,11 @@
+"""
+ASGI config for toddy_shop_backend project.
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'toddy_shop_backend.settings')
+
+application = get_asgi_application()
